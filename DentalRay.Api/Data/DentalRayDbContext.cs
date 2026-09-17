551451898e22cc6d3aa13fa789912964fbbdfdc5
ArjanAraye2 <ArjanAraye2@gmail.com>
2026-09-17 16:47:59 -0400

    Map Users and Staff for authentication

--- DentalRay.Api/Data/DentalRayDbContext.cs
+++ DentalRay.Api/Data/DentalRayDbContext.cs
@@ -3,11 +3,7 @@ using Microsoft.EntityFrameworkCore;
 
 namespace DentalRay.Api.Data
 {
-    // ============================================================
-    // DentalRayDbContext
-    // ============================================================
     // EF Core gateway for the DentalRay database.
-    // ============================================================
     public class DentalRayDbContext : DbContext
     {
         public DentalRayDbContext(DbContextOptions<DentalRayDbContext> options) : base(options) { }
@@ -17,54 +13,29 @@ namespace DentalRay.Api.Data
         public DbSet<RadiologyImage> RadiologyImages { get; set; }
         public DbSet<RadiologyStudyImage> RadiologyStudyImages { get; set; }
         public DbSet<RadiologyStudyTooth> RadiologyStudyTeeth { get; set; }
-
-        // Lookup values used by the Study form.
         public DbSet<StudyType> StudyTypes { get; set; }
+        public DbSet<Staff> Staff { get; set; }
+        public DbSet<User> Users { get; set; }
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
             base.OnModelCreating(modelBuilder);
 
-            modelBuilder.Entity<RadiologyStudy>()
-                .HasOne<Patient>().WithMany()
-                .HasForeignKey(study => study.PatientID)
-                .OnDelete(DeleteBehavior.NoAction);
+            modelBuilder.Entity<RadiologyStudy>().HasOne<Patient>().WithMany().HasForeignKey(x => x.PatientID).OnDelete(DeleteBehavior.NoAction);
+            modelBuilder.Entity<RadiologyStudy>().HasOne<StudyType>().WithMany().HasForeignKey(x => x.StudyTypeID).OnDelete(DeleteBehavior.NoAction);
+            modelBuilder.Entity<RadiologyImage>().HasOne<Patient>().WithMany().HasForeignKey(x => x.PatientID).OnDelete(DeleteBehavior.NoAction);
+            modelBuilder.Entity<RadiologyImage>().HasIndex(x => new { x.PatientID, x.SerialNumber }).IsUnique();
 
-            // Every StudyTypeID must point to a valid row in tblStudyTypes.
-            modelBuilder.Entity<RadiologyStudy>()
-                .HasOne<StudyType>().WithMany()
-                .HasForeignKey(study => study.StudyTypeID)
-                .OnDelete(DeleteBehavior.NoAction);
+            modelBuilder.Entity<RadiologyStudyImage>().HasKey(x => new { x.StudyID, x.ImageID });
+            modelBuilder.Entity<RadiologyStudyImage>().HasOne<RadiologyStudy>().WithMany().HasForeignKey(x => x.StudyID).OnDelete(DeleteBehavior.NoAction);
+            modelBuilder.Entity<RadiologyStudyImage>().HasOne<RadiologyImage>().WithMany().HasForeignKey(x => x.ImageID).OnDelete(DeleteBehavior.NoAction);
 
-            modelBuilder.Entity<RadiologyImage>()
-                .HasOne<Patient>().WithMany()
-                .HasForeignKey(image => image.PatientID)
-                .OnDelete(DeleteBehavior.NoAction);
+            modelBuilder.Entity<RadiologyStudyTooth>().HasKey(x => new { x.StudyID, x.ToothNumber });
+            modelBuilder.Entity<RadiologyStudyTooth>().HasOne<RadiologyStudy>().WithMany().HasForeignKey(x => x.StudyID).OnDelete(DeleteBehavior.NoAction);
 
-            modelBuilder.Entity<RadiologyImage>()
-                .HasIndex(image => new { image.PatientID, image.SerialNumber })
-                .IsUnique();
-
-            modelBuilder.Entity<RadiologyStudyImage>()
-                .HasKey(link => new { link.StudyID, link.ImageID });
-
-            modelBuilder.Entity<RadiologyStudyImage>()
-                .HasOne<RadiologyStudy>().WithMany()
-                .HasForeignKey(link => link.StudyID)
-                .OnDelete(DeleteBehavior.NoAction);
-
-            modelBuilder.Entity<RadiologyStudyImage>()
-                .HasOne<RadiologyImage>().WithMany()
-                .HasForeignKey(link => link.ImageID)
-                .OnDelete(DeleteBehavior.NoAction);
-
-            modelBuilder.Entity<RadiologyStudyTooth>()
-                .HasKey(x => new { x.StudyID, x.ToothNumber });
-
-            modelBuilder.Entity<RadiologyStudyTooth>()
-                .HasOne<RadiologyStudy>().WithMany()
-                .HasForeignKey(x => x.StudyID)
-                .OnDelete(DeleteBehavior.NoAction);
+            // Every DentalRay User belongs to exactly one Staff record.
+            // StaffID and UserName are unique in the SQL schema as well.
+            modelBuilder.Entity<User>().HasOne<Staff>().WithMany().HasForeignKey(x => x.StaffID).OnDelete(DeleteBehavior.NoAction);
         }
     }
 }
